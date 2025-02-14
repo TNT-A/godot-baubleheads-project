0f901fd76a88f451df16a19addb9a0ed57c91cee
grape132 <charlesmargolis2008@gmail.com>
2025-02-14 17:06:10 -0500

    Pause and settings menu
    made a very temporary pause menu, it works, but doesn't really do much

---
+++ testing/testing.gd
@@ -0,0 +1,24 @@
+extends Node2D
+@onready var player_body: CharacterBody2D = $PlayerBody
+@onready var pause_menu: Control = $PlayerBody/PauseMenu
+@onready var settings_menu: Control = $PlayerBody/SettingsMenu
+
+var paused = false
+
+func _ready():
+	pause_menu.hide()
+	settings_menu.hide()
+	
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Pause"):
+		pauseMenu()
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+	
+	paused = !paused
