f2f1680861df52fccc63029820c322365be01e17
TNT-A <andrejr.snellings@gmail.com>
2025-02-14 23:23:17 -0500

    push it real good

--- pause/pause_menu.gd
+++ pause/pause_menu.gd
@@ -1,14 +1,12 @@
 extends Control
 
-@onready var settings_menu: Control = $"../SettingsMenu"
+@onready var pause_menu: Control = %PauseMenu
+@onready var settings_menu: Control = %SettingsMenu
 
 @onready var main = $"../../"
-@onready var pause_menu: Control = $"."
-
 
 func _on_resume_pressed() -> void:
 	main.pauseMenu()
-
 
 func _on_settings_pressed() -> void:
 	settings_menu.show()
