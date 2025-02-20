e3c3d7cf1fbe7ed64c0687fb4ab363a9807e04ec
grape132 <charlesmargolis2008@gmail.com>
2025-02-20 16:34:37 -0500

    projectileStuff
    code is super broken, but I'll fix it later

--- enemies/earthworm/earthworm.gd
+++ enemies/earthworm/earthworm.gd
@@ -1,14 +1,18 @@
 extends CharacterBody2D
 
-var player : Node = Global.player
-
-
-
-#func deal_damage():
-	#
-	#pass
+@onready var player_body: CharacterBody2D = $"."
 
 func _on_damage_zone_body_entered(body: Node2D) -> void:
-	body = player
-	#deal_damage()
-	pass # Replace with function body.
+	body = player_body
+
+
+@export var bullet_scene : PackedScene
+
+func get_input():
+	if Input.is_action_just_pressed("Testing"):
+		shoot()
+		
+func shoot():
+	var b = bullet_scene.instantiate()
+	owner.add_child(b)
+	b.transform = $Mouth.global_transform
