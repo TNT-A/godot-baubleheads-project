9191bbc18beeb16e733a93fd6d34b38408e8c356
grape132 <charlesmargolis2008@gmail.com>
2025-02-25 17:55:36 -0500

    erm

--- bullet.gd
+++ bullet.gd
@@ -6,5 +6,21 @@ var speed = 100
 func _physics_process(delta: float) -> void:
 	position += transform.x * speed * delta
 
+<<<<<<< Updated upstream
 func _on_bullet_body_entered(body: Node2D) -> void:
 	queue_free()
+=======
+func destroy():
+	queue_free()
+	
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
+
+func _on_Bullet_body_entered(body):
+	if body.is_in_group("player"):
+		destroy()
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	pass
+>>>>>>> Stashed changes
