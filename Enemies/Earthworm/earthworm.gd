d985bb62c54c2ac136864ef2821475dc6e7e4b4b
grape132 <charlesmargolis2008@gmail.com>
2025-02-25 17:59:30 -0500

    erm

--- Enemies/Earthworm/earthworm.gd
+++ Enemies/Earthworm/earthworm.gd
@@ -1,18 +1,48 @@
-extends CharacterBody2D
+extends Node2D
 
-@onready var player_body: CharacterBody2D = $"."
+@onready var player_body: Node2D = $"."
 
 func _on_damage_zone_body_entered(body: Node2D) -> void:
 	body = player_body
 
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
-@export var bullet_scene : PackedScene
+@export var BULLET : PackedScene = null
+var target: Node2D = null
+@onready var earthworm: CharacterBody2D = $CharacterBody2D
+@onready var raycast: RayCast2D = $CharacterBody2D/RayCast2D
+@onready var timer: Timer = $CharacterBody2D/RayCast2D/ReloadTimer
 
-func get_input():
-	if Input.is_action_just_pressed("Testing"):
-		shoot()
+func _ready():
+	await(get_tree())
+	target = find_target()
+
+func _physics_process(delta: float) -> void:
+	if target != null:
+		var angle_to_target: float = global_position.direction_to(target.global_position).angle()
+		raycast.global_rotation = angle_to_target
 		
+		if raycast.is_colliding() and raycast.get_collider().is_in_group("player"):
+			earthworm.rotation = angle_to_target
+			if timer.is_stopped():
+				shoot()
+
+
 func shoot():
-	var b = bullet_scene.instantiate()
-	owner.add_child(b)
-	b.transform = $Mouth.global_transform
+	
+	raycast.enabled = false
+	if BULLET: 
+		var bullet: Node2D = BULLET.instance()
+		get_tree().current_scene.add_child(bullet)
+		bullet.global_position = global_position
+		bullet.global_rotation = raycast.global_rotation
+	timer.start()
+
+func find_target():
+	var new_target: Node2D = null
+	if get_tree().has_group("player"):
+		new_target = get_tree().get_nodes_in_group("player")[0]
+	return new_target
+
+func _on_reload_timer_timeout() -> void:
+	raycast.enabled = true
--- bullet.gd
+++ bullet.gd
@@ -6,10 +6,9 @@ var speed = 100
 func _physics_process(delta: float) -> void:
 	position += transform.x * speed * delta
 
-<<<<<<< Updated upstream
 func _on_bullet_body_entered(body: Node2D) -> void:
 	queue_free()
-=======
+
 func destroy():
 	queue_free()
 	
@@ -23,4 +22,3 @@ func _on_Bullet_body_entered(body):
 
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
 	pass
->>>>>>> Stashed changes
