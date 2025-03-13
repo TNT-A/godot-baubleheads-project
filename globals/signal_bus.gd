9a40b257f1c120ecd0f32a0cd1b62e35d5c4904f
TNT-A <andrejr.snellings@gmail.com>
2025-03-13 18:01:45 -0400

    added more AI to baubles, especially attacking

--- globals/signal_bus.gd
+++ globals/signal_bus.gd
@@ -1,6 +1,10 @@
 extends Node
 
+#UI and Inventory
 signal picked_up(item_resource)
 signal item_added(item_resourse, item_slot)
 signal item_used(item_resourse, party_slot)
 signal update_party_slot(item_type, party_slot)
+
+#Enemy Thingamajigs
+signal change_enemy_health(enemy, change)
