d51d8e79745d25619cc6048c40c120a5d6605754
Starflight6041 <electricguitar123456@gmail.com>
2025-03-15 20:16:53 -0400

    crack
    I bought crack

---
+++ Enemies/Earthworm/crackscript.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var move = Vector2(global_position.x - Global.player.global_position.x, global_position.y - Global.player.global_position.y).normalized()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Timer.wait_time = 6
+	$Timer.start() 
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	global_position -= move*4
+
+	pass
+
+
+func _on_timer_timeout():
+	queue_free()
+	pass # Replace with function body.
--- Enemies/Earthworm/earthworm.gd
+++ Enemies/Earthworm/earthworm.gd
@@ -1,31 +1,43 @@
 extends Node2D
 #fix bug: should be called hydra.gd
 @export var health : int = 50
-
+var crackshot: PackedScene = preload("res://Enemies/Earthworm/crackshot.tscn")
 var under_attack : bool = false
 var is_alive : bool = true
-
+var attack_time : bool = false
 enum States {IDLE, ATTACK, MOVE}
-var state: States = States.IDLE
+var state: States = States.ATTACK
+
+
 func _ready():
 	SignalBus.change_enemy_health.connect(change_health)
+	
 
 func _physics_process(delta: float) -> void:
 	if is_alive == false:
 		die()
 	if Input.is_action_just_pressed("Testing"):
 		queue_free()
-	#state_transition()
+	
 	state_functions()
 
-	
+
 	
 func state_functions():
 	if state == States.ATTACK:
+		
 		if $TimerAttackCooldown.is_stopped():
+			
+			$TimerAttackCooldown.wait_time = randf_range(1.2,2.0)
 			$TimerAttackCooldown.start()
-			#$TimerAttackCooldown.wait_time = randf.range(1.2,2.0)
+	if attack_time:
+		shoot()
+		attack_time = false
 func shoot():
+	
+	add_child(crackshot.instantiate())
+	
+	
 	#Global.player.global_position 
 	pass
 func change_health(enemy, change):
@@ -37,5 +49,11 @@ func change_health(enemy, change):
 
 func die():
 	queue_free()
+
+
+
 	
-	
+
+
+func _on_timer_attack_cooldown_timeout():
+	attack_time = true
