aa09f5d3e030ce2d44ef0605ef7ed27baa9cd88e
Starflight6041 <electricguitar123456@gmail.com>
2025-06-01 19:27:13 -0400

    sa
    sa

--- enemies/earthworm/crackshot.gd
+++ enemies/earthworm/crackshot.gd
@@ -1,5 +1,6 @@
 extends Node2D
 var mo = Vector2()
+
 @onready var move = (Vector2(global_position.x - Global.player.global_position.x, global_position.y - Global.player.global_position.y) + mo).normalized()
 # Called when the node enters the scene tree for the first time.
 
@@ -16,3 +17,14 @@ func _physics_process(delta: float) -> void:
 
 func _on_timer_timeout():
 	queue_free()
+
+
+func _on_damage_zone_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	if area.is_in_group("player"):
+		
+		var ex = preload("res://effects/explosion.tscn").instantiate()
+		get_parent().add_child(ex)
+		ex.global_position = global_position
+		queue_free()
+	
+	pass # Replace with function body.
