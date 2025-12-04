7a7c55d6c48f5ba5e82771d235107625ccc36ee2
TNT-A <andrejr.snellings@gmail.com>
2025-12-03 20:07:16 -0500

    Adjusted the throw
    Now the throw works on some odd speed math that will calculate how quickly the bauble needs to move in order to sync up it's throw movement and throw animation. It feels much better than before, but at this point I need to do the math to create the throw curve rather than relying on a jank animation for it.

--- baubles/bauble_states/bauble_thrown.gd
+++ baubles/bauble_states/bauble_thrown.gd
@@ -12,10 +12,17 @@ var throw_damage : int = 2
 var range : float = 0.4
 var bauble_body : CharacterBody2D 
 
+var throw_speed : float = 500
+var base_throw_speed : float = 500
+
+#How throw?
+# 1. Click on where you want the bauble to go.
+# 2. The bauble will go in that direction and stop if either:
+# 	 The bauble gets tossed out of range
+# 	 The bauble reaches wherever the mouse landed.
+
 func enter():
 	bauble_body = get_parent().get_parent().body
-	$ThrowTimer.wait_time = range
-	$ThrowTimer.start()
 	damage_active = true
 	throw_target = Vector2(0,0)
 	parent_body.can_throw = false
@@ -24,28 +31,37 @@ func enter():
 	pathfinding_controller = parent_body.find_child("Pathfinding")
 	body_detector = parent_body.find_child("BodyDetector")
 	animation_player = pathfinding_controller.host.find_child("AnimationPlayer")
-	#animation_player.play("throw")
+	animation_player.play("throw")
 	body_detector.clear_body()
 	pathfinding_controller.host.global_position = pathfinding_controller.target_node.global_position
 	throw_target = get_global_mouse_position()
+	set_speed()
+	$ThrowTimer.wait_time = range
+	$ThrowTimer.start()
+
+func set_speed():
+	var dist = abs(Global.player.global_position.distance_to(throw_target))
+	throw_speed = dist / range
 
 func physics_update(delta):
 	check_transitions()
 	pathfinding_controller.active = false
 	var direction = (throw_target - player.global_position).normalized()
+	pathfinding_controller.host.velocity = direction * throw_speed
 	
-	if bauble_body.global_position.distance_to(throw_target) >= bauble_body.global_position.distance_to(throw_target)/2:
-		pathfinding_controller.host.velocity = direction * lerp(100, 500, 0.5)
-	else:
-		pathfinding_controller.host.velocity = direction * lerp(500, 350, 0.2)
+	#if bauble_body.global_position.distance_to(throw_target) >= bauble_body.global_position.distance_to(throw_target)/2:
+		#pathfinding_controller.host.velocity = direction * lerp(100.0, throw_speed, 0.5)
+	#else:
+		#pathfinding_controller.host.velocity = direction * lerp(throw_speed, throw_speed/2, 0.2)
 	
 	#get_parent().get_parent().body.global_position = get_parent().get_parent().body.global_position.lerp(throw_target, 0.2)
 	#body_detector.active = true
 	#if body_detector.enemy_found and damage_active and is_instance_valid(body_detector.tracked_body):
 		#SignalBus.emit_signal("change_enemy_health", body_detector.tracked_body, throw_damage)
 		#damage_active = false
-	if bauble_body.global_position.distance_to(throw_target) <= 5:
-		end_throw()
+	
+	#if bauble_body.global_position.distance_to(throw_target) <= 5:
+		#end_throw()
 
 func throw():
 	throw_started = true
